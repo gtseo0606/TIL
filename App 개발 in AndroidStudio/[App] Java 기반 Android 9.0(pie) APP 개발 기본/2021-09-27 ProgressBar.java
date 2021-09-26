1eca9f49a0b0c1ce56c8892b9b276e2e055cacb1
gtseo0606 <74644453+gtseo0606@users.noreply.github.com>
2021-09-27 00:28:13 +0900

    Create 2021-09-27 ProgressBar.java

---
+++ App 개발 in AndroidStudio/[App] Java 기반 Android 9.0(pie) APP 개발 기본/2021-09-27 ProgressBar.java
@@ -0,0 +1,31 @@
+package kr.co.softcampus.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    //참조변수
+    ProgressBar bar4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bar4 = (ProgressBar)findViewById(R.id.progressBar4);
+    }
+
+    public void btn1Method(View view){
+        bar4.incrementProgressBy(5);
+    }
+    public void btn2Method(View view){
+        bar4.incrementProgressBy(-5);
+    }
+    public void btn3Method(View view){
+        bar4.setProgress(80);
+    }
+}
