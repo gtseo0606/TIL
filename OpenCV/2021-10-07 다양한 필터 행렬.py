8705d717acab9a5a6522cc9d890cb7ed00ffa7a7
gtseo0606 <74644453+gtseo0606@users.noreply.github.com>
2021-10-07 13:49:11 +0900

    Create 2021-10-07 다양한 필터 행렬.py

---
+++ OpenCV/2021-10-07 다양한 필터 행렬.py
@@ -0,0 +1,9 @@
+opencv 다양한 필터 행렬
+
+# cv2.getStructuringElement()
+
+cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+
+cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+
+cv2.getStructuringElement(cv2.MORPH_CROSS, (5,5))
